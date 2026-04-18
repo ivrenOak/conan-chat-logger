20b10f7d3d9e27e11b647c8934ba632abba10bd0
ivren <ivren>
2026-04-18 12:36:15 +0200

    feat: initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,8 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
-
 // https://vitejs.dev/config
 export default defineConfig({
-    root: 'app',
-    build: {
-        rollupOptions: {
-            input: 'app/index.html',
-        },
-    },
     plugins: [react(), tailwindcss()],
     resolve: {
         alias: {
